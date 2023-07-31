e447b155575c95ac350983e15da36c9f63f79c66
Conor McLaughlin <cfm5qc@virginia.edu>
2023-07-31 12:04:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/cmclaugh3/booklover',
     author='Conor McLaughlin',
     description='Package containing Python Scripts for DS5100 hw09',
-    packages=find_packages().
+    packages=find_packages(),
     license='MIT',
     install_requires=['pandas >= 0.20.0']
 )
